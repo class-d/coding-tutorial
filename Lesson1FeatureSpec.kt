8cb754685e3bbf1c7e466dba9e706f3eb355c1ba
class-d <47152173+class-d@users.noreply.github.com>
2019-01-30 21:21:57 +0600

    Изменил minOf , describe

---
+++ Lesson1FeatureSpec.kt
@@ -0,0 +1,111 @@
+import io.kotlintest.matchers.collections.shouldContain
+import io.kotlintest.matchers.numerics.shouldBeLessThan
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.FeatureSpec
+
+class BasicsFeatureSpec : FeatureSpec({
+    feature("functions") {
+        scenario("sum1 and sum2 works the same") {
+            val a = 1
+            val b = 2
+            sum1(a, b) shouldBe 3
+            sum1(a, b) shouldBeLessThan 4
+            sum1(0, 3) shouldBe 3
+            sum1(-1, 1) shouldBe 0
+            // Add greater less checks
+        }
+    }
+
+    feature("variables") {
+
+        val readOnly = 11
+        var reassignable = 3
+//            scenario("val can not be reassigned") {
+//                readOnly = 12
+//            }
+
+        scenario("can be reassigned") {
+            reassignable = 6
+            reassignable shouldBe 6
+        }
+    }
+
+    feature("strings") {
+        val toge = "toge"
+        val ther = "ther"
+        val together = "together"
+
+        scenario("concatenation works") {
+            toge + ther shouldBe together
+        }
+
+        scenario("string interpolation works") {
+            "$toge$ther" shouldBe together
+        }
+
+        scenario("is not empty") {
+            together.isNotBlank() shouldBe true
+        }
+    }
+
+    feature("conditional expressions") {
+        val max = 100
+        val min = 0
+
+        scenario("returns max") {
+            maxOf(min, max) shouldBe max
+        }
+
+        scenario("") {
+            minOf(1, 2)
+        }
+    }
+
+    // Write minOff function
+
+    feature("when expression") {
+        describe(10) shouldBe "Ten"
+        describe("Bye") shouldBe "Parting"
+         // Add other checks
+    }
+    feature("collections") {
+        val fruits = arrayListOf("Apple", "Orange", "Grapes", "Cherry")
+
+        scenario("") {
+            fruits.count() shouldBe 4
+            fruits shouldContain "Apple"
+
+            count(fruits) shouldBe 4
+        }
+    }
+})
+
+fun sum1(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun sum2(a: Int, b: Int) = a + b
+
+fun maxOf(a: Int, b: Int) = if (a > b) a else b
+
+fun minOf(x: Long, y: Long): Any {
+    if (x < y) return x
+    return Unit
+}
+
+fun describe(obj: Any): String =
+        when (obj) {
+            10 -> "Ten"
+            "Bye" -> "Parting"
+            is Short -> "Short"
+            !is Long -> "Not a long"
+            else -> "Unknown"
+        }
+
+fun count(list: ArrayList<String>): Int {
+    var counter = 0
+    for (i in list) {
+        counter += 1
+    }
+    return counter
+}
