ab52d2d5af1d64db8711643869c7af00d5029a58
class-d <47152173+class-d@users.noreply.github.com>
2019-02-13 20:46:49 +0600

    Add files via upload

--- homework.kts
+++ homework.kts
@@ -1,4 +1,27 @@
-val fruits = listOf("bananas", "apple", "orange")
-val map = mapOf("bananas" to 1, "apple" to 2,"orange" to 3)
-println(map.mapValues { it.value.toUpperCase() })
-{"bananas"=1,"apple"=2,"orange"=3}
+import com.sun.xml.internal.ws.api.pipe.Engine
+import javax.print.attribute.standard.MediaSize
+import io.kotlintest.runner.jvm.TestDiscovery.loadClasses(TestDiscovery.kt:67)
+import io.kotlintest.runner.jvm.TestDiscovery.scan(TestDiscovery.kt:73)
+import io.kotlintest.runner.jvm.TestDiscovery.discover(TestDiscovery.kt:101)
+import io.kotlintest.runner.junit5.KotlinTestEngine.discover(KotlinTestEngine.kt:80)
+import io.kotlintest.runner.junit5.KotlinTestEngine.discover(KotlinTestEngine.kt:31)
+import org.junit.platform.launcher.core.DefaultLauncher.discoverEngineRoot(DefaultLauncher.java:130)
+import org.junit.platform.launcher.core.DefaultLauncher.discoverRoot(DefaultLauncher.java:117)
+import org.junit.platform.launcher.core.DefaultLauncher.execute(DefaultLauncher.java:90)
+import com.intellij.junit5.JUnit5IdeaTestRunner.startRunnerWithArgs(JUnit5IdeaTestRunner.java:74)
+import com.intellij.rt.execution.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:47)
+import com.intellij.rt.execution.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:242)
+import com.intellij.rt.execution.junit.JUnitStarter.main(JUnitStarter.java:70)
+class Coder () {
+    const val = 100
+    val linesOfCode = 0
+    val write = 3 hour
+    fun write = linesOfCode + 100 - 3 hour
+}
+class Developer () : Coder () {
+    fun write = linesOfCode - bugs
+}
+class QA () {
+    const work = bugs
+    fun work = linesOfCode + bugs
+}
